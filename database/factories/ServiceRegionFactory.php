62d409e0b37280dad0fc1d242a57899f85d6f03d
Matt Stauffer <matt@tighten.co>
2019-03-06 10:14:58 -0500

    Smaller, square service regions

--- database/factories/ServiceRegionFactory.php
+++ database/factories/ServiceRegionFactory.php
@@ -16,26 +16,34 @@ $factory->define(App\ServiceRegion::class, function (Faker $faker) {
             $pointOne[0] += $faker->randomFloat(2, -1, 1);
             $pointOne[1] += $faker->randomFloat(2, -1, 1);
 
-            // Go east one quarter degree
+            // Go east one eighth degree
             $pointTwo = [
                 $pointOne[0] + 0,
-                $pointOne[1] + 0.25,
+                $pointOne[1] + 0.125,
             ];
 
-            // Go south one degree
+            // Go south one eighth degree
             $pointThree = [
-                $pointOne[0] - 0.25,
-                $pointOne[1] + 0.25,
+                $pointOne[0] - 0.125,
+                $pointOne[1] + 0.125,
+            ];
+
+            // Go west one eighthdegree
+            $pointFour = [
+                $pointOne[0] - 0.125,
+                $pointOne[1] + 0,
             ];
 
             return DB::raw(sprintf(
-                "ST_PointFromText('POLYGON((%s %s, %s %s, %s %s, %s %s))', 4326)",
+                "ST_PointFromText('POLYGON((%s %s, %s %s, %s %s, %s %s, %s %s))', 4326)",
                 $pointOne[0],
                 $pointOne[1],
                 $pointTwo[0],
                 $pointTwo[1],
                 $pointThree[0],
                 $pointThree[1],
+                $pointFour[0],
+                $pointFour[1],
                 $pointOne[0],
                 $pointOne[1]
             ));
